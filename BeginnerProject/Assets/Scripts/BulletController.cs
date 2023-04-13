58e49f3a22beff29cc43cf69ff4d8ec73f0b2cdb
Dimovskii <anddimovski@gmail.com>
2023-04-13 13:04:31 +0400

    updated class BulletController

--- BeginnerProject/Assets/Scripts/BulletController.cs
+++ BeginnerProject/Assets/Scripts/BulletController.cs
@@ -3,8 +3,25 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     private float _lifeTime = 3f;
-    private void Start() 
+    private int _damageCount = 10;
+    private 
+    WeaponManager _weaponManager;
+    
+    private void Awake() 
     {
-        Destroy(gameObject, _lifeTime);    
+        Destroy(gameObject, _lifeTime);
+    }
+
+    private void Update() 
+    {
+       Debug.Log(_damageCount);
+    }
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if(collision.gameObject.TryGetComponent<EnemyHealth>(out EnemyHealth enemy))
+        {
+            enemy.TakeDamage(_damageCount);
+        }
     }
 }
