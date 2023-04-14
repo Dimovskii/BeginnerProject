42c25c09322778ca6a839c62fd4c158ec9d0225a
Dimovskii <anddimovski@gmail.com>
2023-04-14 19:21:07 +0400

    fix class PlayerHealt

--- BeginnerProject/Assets/Scripts/PlayerHealth.cs
+++ BeginnerProject/Assets/Scripts/PlayerHealth.cs
@@ -8,28 +8,10 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     private const int _healAmount = 20;
     private int _startHealthAmount = 100;
     private int _maxHealthAmount = 100;
-    public event Action<int> OnHealed;
-    public event Action OnDead;
-    [SerializeField] GameOverScreen _gamerOverScreen;
     
     private void Awake() 
     {
         _playerHealth = new Health(_startHealthAmount,_maxHealthAmount);
-    }
-
-    private void Update()   
-    {
-        HealthChange();
-        Debug.Log($"PlayerHealth" + _playerHealth.CurrentHealth);
-
-    }
-    private void HealthChange()
-    {
-        if (Input.GetKeyDown(KeyCode.H))
-        {   
-            OnHealed?.Invoke(_healAmount);
-            Debug.Log(_playerHealth.CurrentHealth);
-        }
     }
 
     private void Heal(int healing)
@@ -39,22 +21,17 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 
     public void TakeDamage(int damage)
     {
-        Debug.Log(_playerHealth.CurrentHealth);
         if(_playerHealth.CurrentHealth > 0)
+        {
             _playerHealth.Damage(damage);
-        else
-            OnDead?.Invoke();
-
+        }
+        else if (_playerHealth.CurrentHealth == 0)
+        {
+            Dead();
+        }
     }
-
-    private void OnEnable() 
+    private void Dead()
     {
-        OnHealed += Heal;
-        OnDead += _gamerOverScreen.ShowGameOverScreen;
-    }
-    private void OnDisable() 
-    {
-        OnHealed -= Heal;
-        OnDead -= _gamerOverScreen.ShowGameOverScreen;
+        throw new NotImplementedException();
     }
 }
