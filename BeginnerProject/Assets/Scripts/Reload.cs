8d49017743a4ca3c5d9dcc5d1b5d11f48dd2e458
Dimovskii <anddimovski@gmail.com>
2023-05-04 12:36:07 +0400

    added class Reload

---
+++ BeginnerProject/Assets/Scripts/Reload.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Reload : MonoBehaviour
+    {
+        private InputHandler _inputHandler;
+        private WeaponSwitching _weaponSwitching;
+        private WeaponData _weaponData;
+        private bool _isReloading = false;
+
+        private void Awake()
+        {
+            _inputHandler = GetComponent<InputHandler>();
+            _weaponSwitching = GetComponent<WeaponSwitching>();
+        }
+
+        private void OnEnable()
+        {
+            _weaponSwitching.OnWeaponDataChanged += GetWeaponData;
+            _inputHandler.OnReloaded += StartReloading;
+        }
+
+        private void GetWeaponData(WeaponData data)
+        {
+            _weaponData = data;
+        }
+
+        private void StartReloading()
+        {
+            _isReloading = true;
+            StartCoroutine(Reloading());
+        }
+
+        IEnumerator Reloading()
+        {
+            while (_isReloading)
+            {
+                yield return new WaitForSeconds(_weaponData.ReloadTime);
+                _weaponData.CurrentAmmoCount = _weaponData.MaxAmmoCount;
+                _isReloading = false;
+            }
+        }
+
+        private void OnDisable()
+        {
+            _weaponSwitching.OnWeaponDataChanged -= GetWeaponData;
+            _inputHandler.OnReloaded -= StartReloading;
+        }
+    }
+}
