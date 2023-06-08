a6fc7bdf446c5cbab30ed4915dd18eadb33760d6
Dimovskii <anddimovski@gmail.com>
2023-06-08 14:53:52 +0400

    created Bootstrap prefab updated class Bootstrap

--- BeginnerProject/Assets/Scripts/Bootstrap.cs
+++ BeginnerProject/Assets/Scripts/Bootstrap.cs
@@ -1,34 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;      
 
 public class Bootstrap : MonoBehaviour
 {
-    public static Bootstrap Instance;
-    [SerializeField] private Camera _uiCamera;
-    [SerializeField] private GameObject _mainMenu;
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-
-        GetUICamera();
-        GetMainMenu();
-
-    }
-
-    private void GetUICamera()
-    {
-        _uiCamera = Instantiate(_uiCamera);
-    }
-
-    private void GetMainMenu()
-    {
-        _mainMenu = Instantiate(_mainMenu);
+        SceneManager.LoadSceneAsync(EnumScenes.MainMenu.ToString());
+        DontDestroyOnLoad(gameObject);  
     }
 }
