d7231e866ee52064f2e88275d20eaa7f786f73b1
Dimovskii <anddimovski@gmail.com>
2023-06-08 15:46:57 +0400

    updated class CameraFollower

--- BeginnerProject/Assets/Scripts/Camera/CameraFollower.cs
+++ BeginnerProject/Assets/Scripts/Camera/CameraFollower.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    [SerializeField] Transform _target;
-    [SerializeField] float _smoothSpeed = 0.1f;
-    [SerializeField] private Vector3 _offset;
+    private float _smoothSpeed = 0.1f;
+    private Vector3 _offset;
+    public Transform Player { get ; set ; }
 
-    public void Init(Transform target) 
+    private void Awake()
     {
         _offset = new Vector3(0, 70, -74);
-        _target = target;
     }
 
     private void LateUpdate() 
@@ -19,8 +18,11 @@ public class CameraFollower : MonoBehaviour
 
     private void CameraFollow()
     {
-        var desiredPosition = _target.position + _offset;
-        var smootedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
-        transform.position = smootedPosition;
+        if(Player != null)
+        {
+            var desiredPosition = Player.position + _offset;
+            var smootedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+            transform.position = smootedPosition;
+        }
     }
 }
