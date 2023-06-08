f39d046d46b40bceeb180a11f0995272d9343e24
Dimovskii <anddimovski@gmail.com>
2023-06-08 15:48:43 +0400

    added class PrefabFactory and GameConstants

---
+++ BeginnerProject/Assets/Scripts/Factory/GameConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Factory
+{
+    public static class GameConstants
+    {
+        public static readonly string Player = "Player";
+        public static readonly string Ground = "Ground";
+        public static readonly string Camera = "Camera";
+        public static readonly string Enemy = "Enemy";
+        public static readonly string UIRoot = "UIRoot";
+        public static readonly string Hud = "HUD";
+        public static readonly string MainMenu = "MainMenu";
+        public static readonly string PauseMenuScreen = "PauseMenuScreen";
+        public static readonly string GameOverScreen = "GameOverScreen";
+
+    }
+}
---
+++ BeginnerProject/Assets/Scripts/Factory/PrefabFactory.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Factory;
+
+public class PrefabFactory : MonoBehaviour
+{
+    [SerializeField] private GameObject _ground;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject _uiRoot;
+
+    private Dictionary<string, GameObject> _gameObjects;
+    
+    private void Awake()
+    {
+        _gameObjects = new Dictionary<string, GameObject>
+        {
+            { GameConstants.Ground, _ground },
+            { GameConstants.Player, _player },
+            { GameConstants.Camera, _camera },
+            { GameConstants.Enemy, _enemy },
+            { GameConstants.UIRoot, _uiRoot }
+        };
+    }
+
+    public GameObject Create(string key)
+    {
+        if (!_gameObjects.TryGetValue(key, out GameObject value))
+        {
+            Debug.LogError($"Prefab with key {key} not found.");
+            return null;
+        }
+        else
+        {
+            return Instantiate(value);
+        }
+    }
+}
